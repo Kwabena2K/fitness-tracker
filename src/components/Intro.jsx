2f19af2c0c590f970ddb4c9fff30d69d741a3053
Kwabena <kwabenao2k@gmail.com>
2023-03-01 21:19:35 -0500

    Created intro and added icons

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleInfo,
+  faHeart,
+  faThumbsUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Intro() {
   return (
@@ -9,12 +15,18 @@ function Intro() {
         LogMyFitness is designed to replace your paper workout journal.
       </p>
       <p className="flex items-center justify-center flex-col text-center pt-20 pb-6 px-40">
+        <p>
+          <FontAwesomeIcon icon={faCircleInfo} size="2x" color="dodgerblue" />
+        </p>
         Welcome to LogMyFitness! As soon as you arrive, you'll notice that we've
         kept things simple and minimalist, so that you can focus on what matters
         most: your fitness goals. One of the key features of our app is the
         ability to create your own workout logs, which you can then share with
         others via a list.
         <p className="pt-20">
+          <p>
+            <FontAwesomeIcon icon={faThumbsUp} size="2x" color="dodgerblue" />
+          </p>
           Whether you want to keep your logs private, or share them publicly to
           inspire others, our app makes it easy to stay accountable and
           motivated. Adding exercises to your workout log is also a breeze. You
@@ -22,6 +34,9 @@ function Intro() {
           without any fuss or hassle.
         </p>
         <p className="pt-20">
+          <p>
+            <FontAwesomeIcon icon={faHeart} size="2x" color="dodgerblue" />
+          </p>
           If you're looking for a fitness tracker app that is both effective and
           user-friendly, look no further than our app. With its minimalist
           design and powerful features, it's the perfect tool for anyone who
